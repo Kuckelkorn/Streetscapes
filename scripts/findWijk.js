71cdf52fc2e1ca2c33db24f154f79db2db01b1eb
Kuckelkorn <remco.kuckelkorn@soverin.net>
2022-01-24 15:04:00 +0100

    update file structure

--- scripts/findWijk.js
+++ scripts/findWijk.js
@@ -1,15 +1,12 @@
-import createCard from './helpers/createCard.js'
-import splitString from './helpers/splitID.js'
-import getBuurten from "./findBuurt.js"
 import createMap from './helpers/createMap.js'
-import createData from './helpers/createData.js'
+import getData from './helpers/getData.js'
 
 async function getWijken(){
   // const wijken = fetch(`https://api.data.amsterdam.nl/v1/gebieden/wijken/?_format=geojson&ligtInStadsdeel.identificatie=${stadsdeel}`)
   const wijken = await fetch(`https://api.data.amsterdam.nl/v1/gebieden/wijken/?_format=geojson`)
   const geoWijken = await wijken.json()
   geoWijken.features.map(async function(obj){
-    createData(obj.properties.code)
+    getData(obj.properties.code)
   })
   createMap(await geoWijken)
 }
