5d9293a3a4df2513c4f6a50c9203f604dd7b47ba
Kuckelkorn <remco.kuckelkorn@soverin.net>
2022-01-31 09:24:37 +0100

    fix legend

--- scripts/helpers/createBar.js
+++ scripts/helpers/createBar.js
@@ -1,6 +1,4 @@
-// import * as d3 from 'd3'
-
-const createBarChart = (el, data) => {
+const createBarChart = async (el, data) => {
   const margin = {top: 40, bottom: 10, left: 80, right: 10}
   const width = 500 - margin.left - margin.right
   const height = 300 - margin.top - margin.bottom
@@ -10,12 +8,12 @@ const createBarChart = (el, data) => {
   .attr('height', height+margin.top+margin.bottom)
 
   // Create groups in the svg and make sure that the margins are the same this is necessary otherwise the legend is not there
-  const g = svg.append('g')
+  let g = svg.append('g')
   .attr('transform', `translate(${margin.left},${margin.top})`)
 
   // Define the x and y axes and their size
   const xScale = d3.scaleLinear().range([0, width])
-  const yScale = d3.scaleBand().range([0, height]).padding(0.3)
+  const yScale = d3.scaleBand().range([0, height]).padding(0.1)
 
   const yAxis = d3.axisLeft().scale(yScale)
   const g_yAxis = g.append('g').attr('class','y axis')
@@ -23,19 +21,38 @@ const createBarChart = (el, data) => {
   const xAxis = d3.axisTop().scale(xScale)
   const g_xAxis = g.append('g').attr('class','x axis')
 
-  function update(data){
+  const chart = document.querySelector(el)
+
+  // let data
+
+  // d3.json(data).then((json) => {
+  //   data = json
+  //   update(data)
+  // })
+
+  if (chart.childElementCount > 1){
+    chart.removeChild(chart.getElementsByTagName('g')[0])
+    update( await data)
+  } else {
+    update (await data)
+  }
+  
+
+  function update(new_data){
+
     // Defining how to display the data in the dom
-    xScale.domain([0, d3.max(data, (d) => d.waarde)])
-    yScale.domain(data.map((d) => d.indicator))
+    xScale.domain([0, d3.max(new_data, (d) => d.waarde)])
+    yScale.domain(new_data.map((d) => d.indicator))
 
     g_yAxis.transition().call(yAxis)
     g_xAxis.transition().call(xAxis)
 
     // making the individual bars
-    const rect = g.selectAll('rect').data(data).join(
-      (enter) => { enter = enter.append('rect').attr('x', 0)
-      enter.append('title')
-      return enter
+    const rect = g.selectAll('rect').data(new_data, (d) => d.indicator).join(
+      (enter) => { 
+        const rect_enter = enter.append('rect').attr('x', 0)
+        rect_enter.append('title')
+        return rect_enter
       },
       (update) => update,
       (exit) => exit.remove()
@@ -45,12 +62,11 @@ const createBarChart = (el, data) => {
     rect
       .transition()
       .attr('height', yScale.bandwidth())
-      .attr('width', (data) => xScale(data.waarde))
-      .attr('y', (data) => yScale(data.indicator))
+      .attr('width', (new_data) => xScale(new_data.waarde))
+      .attr('y', (new_data) => yScale(new_data.indicator))
       .attr('fill', "#FEC813" )
-      .select('title').text((data) => `${data.waarde}`)
+      .select('title').text((new_data) => `${new_data.indicator}: ${new_data.waarde}`)
   }
-  update(data)
 }
 
 export default createBarChart
